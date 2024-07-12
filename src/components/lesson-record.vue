b905a8a3df88a47489dbcf6f98d9375c26be48ad
dingzhen <dingzhen@qq.com>
2024-07-12 19:06:49 +0800

    feat: second version

--- src/components/lesson-record.vue
+++ src/components/lesson-record.vue
@@ -35,17 +35,43 @@
         <div slot="header" class="clearfix">
           <span>昨日课程反馈记录</span>
           <el-button style="float: right; padding: 3px 0" type="text"
-            >操作按钮</el-button
+            >真的服了</el-button
           >
         </div>
         <div
-          v-for="(record, index) in lessonRecordList"
+          v-for="(record, index) in renderList[yesterday]"
           :key="index"
           class="text item"
           style="margin-top: 20px"
         >
           <el-checkbox v-model="record.isSend" @change="changeCheckBox">
             <el-tag
+              >{{ record.time }}
+              <span style="margin-left: 10px">
+                {{ record.stuOrClass }}
+              </span></el-tag
+            >
+          </el-checkbox>
+        </div>
+      </el-card>
+    </div>
+
+    <div class="student-list">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>今日课程反馈记录</span>
+          <el-button style="float: right; padding: 3px 0" type="text"
+            >真的服了</el-button
+          >
+        </div>
+        <div
+          v-for="(record, index) in renderList[today]"
+          :key="index"
+          class="text item"
+          style="margin-top: 20px"
+        >
+          <el-checkbox v-model="record.isSend" @change="changeCheckBox">
+            <el-tag type="warning"
               >{{ record.time }}
               <span style="margin-left: 10px">
                 {{ record.stuOrClass }}
@@ -72,6 +98,7 @@ export default {
       access_token: "",
       CAName: "吴彬",
       metaBaseInput: "",
+      today: "",
       yesterday: "",
       options: [
         {
@@ -96,6 +123,9 @@ export default {
         },
       ],
       lessonRecordList: [],
+      lessonNextRecordList: [],
+      metaBaseTodayInput: "",
+      renderList: {},
     };
   },
   methods: {
@@ -145,67 +175,90 @@ export default {
     },
     clickFn() {
       const dataAfterParase = JSON.parse(this.metaBaseInput);
+      const dataAfterParaseT = JSON.parse(this.metaBaseTodayInput);
 
       const listAfterFilterByCA = dataAfterParase.filter((item) => {
         return item["助教"] === this.CAName;
       });
+      const listAfterFilterByCAT = dataAfterParaseT.filter((item) => {
+        return item["助教"] === this.CAName;
+      });
+
       const dataAfterSorted = this.sortByPropertyOrder(
         listAfterFilterByCA,
         "学生/班级"
       );
-      const today = moment().format("YYYY-MM-DD");
-      const yesterday = moment().subtract(1, "days").format("YYYY-MM-DD");
-      const localYesterday = localStorage.getItem("yesterday");
-      if (
-        yesterday === localYesterday &&
-        localStorage.getItem(`${this.CAName}lesson-record`)
-      ) {
-        this.lessonRecordList = JSON.parse(
-          localStorage.getItem(`${this.CAName}lesson-record`)
-        );
-        console.log("1");
-      } else {
-        localStorage.setItem("yesterday", yesterday);
+      const dataAfterSortedT = this.sortByPropertyOrder(
+        listAfterFilterByCAT,
+        "学生/班级"
+      );
 
-        localStorage.setItem("CAName", this.CAName);
-        console.log(
-          (this.lessonRecordList = JSON.parse(
-            localStorage.getItem(`${this.CAName}lesson-record`)
-          ))
-        );
-        this.lessonRecordList = dataAfterSorted.map((item) => {
-          return {
-            time: `${item.start.slice(-5)}-${item.end.slice(-5)}`,
-            subject: item["课程"],
-            stuOrClass: item["学生/班级"],
-            teacher: item["教师"],
-            isSend: item["isSend"] ? true : false,
-          };
-        });
+      this.today = moment().format("YYYY-MM-DD");
+      this.yesterday = moment().subtract(1, "days").format("YYYY-MM-DD");
+
+      const transferY = dataAfterSorted.map((item) => {
+        return {
+          time: `${item.start.slice(-5)}-${item.end.slice(-5)}`,
+          subject: item["课程"],
+          stuOrClass: item["学生/班级"],
+          teacher: item["教师"],
+          isSend: item["isSend"] ? true : false,
+        };
+      });
+
+      const transferT = dataAfterSortedT.map((item) => {
+        return {
+          time: `${item.start.slice(-5)}-${item.end.slice(-5)}`,
+          subject: item["课程"],
+          stuOrClass: item["学生/班级"],
+          teacher: item["教师"],
+          isSend: item["isSend"] ? true : false,
+        };
+      });
+
+      if (!localStorage.getItem(`${this.CAName}lesson-course`)) {
+        this.renderList = {
+          [this.yesterday]: transferY,
+          [this.today]: transferT,
+        };
         localStorage.setItem(
-          `${this.CAName}lesson-record`,
-          JSON.stringify(this.lessonRecordList)
+          `${this.CAName}lesson-course`,
+          JSON.stringify(this.renderList)
         );
+      } else {
+        const localData = JSON.parse(
+          localStorage.getItem(`${this.CAName}lesson-course`)
+        );
+        this.renderList = {
+          [this.yesterday]: localData[this.yesterday] ?? transferY,
+          [this.today]: localData[this.today] ?? transferT,
+        };
+        localStorage.setItem(
+          `${this.CAName}lesson-course`,
+          JSON.stringify(this.renderList)
+        );
+        console.log("this.renderList", this.renderList);
       }
     },
     async getRecord() {
-      axios
-        .get(
-          "/class/json?parameters=%5B%7B%22type%22%3A%22category%22%2C%22value%22%3A%5B%22%E5%8D%8A%E6%B5%B7%E4%BA%BA%E5%B9%BF%22%5D%2C%22id%22%3A%22e7ab001d-adfb-44aa-7cd3-96ee5f8d0dc2%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22dept%22%5D%5D%7D%2C%7B%22type%22%3A%22date%2Fall-options%22%2C%22value%22%3A%22past1days%22%2C%22id%22%3A%22953e6c0e-7467-721c-8065-fa3451526c25%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22template-tag%22%2C%22date%22%5D%5D%7D%2C%7B%22type%22%3A%22category%22%2C%22value%22%3Anull%2C%22id%22%3A%22627547e3-e078-8cd7-1c44-0038eba685e3%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22stuName%22%5D%5D%7D%5D&format_rows=true"
-        )
-        .then((response) => {
-          this.metaBaseInput = JSON.stringify(response.data);
-          this.$message.success("自动获取昨日课表成功~");
-          this.clickFn();
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      const responseY = await axios.get(
+        "/class/json?parameters=%5B%7B%22type%22%3A%22category%22%2C%22value%22%3A%5B%22%E5%8D%8A%E6%B5%B7%E4%BA%BA%E5%B9%BF%22%5D%2C%22id%22%3A%22e7ab001d-adfb-44aa-7cd3-96ee5f8d0dc2%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22dept%22%5D%5D%7D%2C%7B%22type%22%3A%22date%2Fall-options%22%2C%22value%22%3A%22past1days%22%2C%22id%22%3A%22953e6c0e-7467-721c-8065-fa3451526c25%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22template-tag%22%2C%22date%22%5D%5D%7D%2C%7B%22type%22%3A%22category%22%2C%22value%22%3Anull%2C%22id%22%3A%22627547e3-e078-8cd7-1c44-0038eba685e3%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22stuName%22%5D%5D%7D%5D&format_rows=true"
+      );
+
+      const responseT = await axios.get(
+        "/class/json?parameters=%5B%7B%22type%22%3A%22category%22%2C%22value%22%3A%5B%22%E5%8D%8A%E6%B5%B7%E4%BA%BA%E5%B9%BF%22%5D%2C%22id%22%3A%22e7ab001d-adfb-44aa-7cd3-96ee5f8d0dc2%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22dept%22%5D%5D%7D%2C%7B%22type%22%3A%22date%2Fall-options%22%2C%22value%22%3A%22thisday%22%2C%22id%22%3A%22953e6c0e-7467-721c-8065-fa3451526c25%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22template-tag%22%2C%22date%22%5D%5D%7D%2C%7B%22type%22%3A%22category%22%2C%22value%22%3Anull%2C%22id%22%3A%22627547e3-e078-8cd7-1c44-0038eba685e3%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22stuName%22%5D%5D%7D%5D&format_rows=true"
+      );
+
+      this.metaBaseInput = JSON.stringify(responseY.data);
+      this.metaBaseTodayInput = JSON.stringify(responseT.data);
+
+      this.$message.success("自动获取昨日、今日课表成功~");
+      this.clickFn();
     },
     changeCheckBox() {
       localStorage.setItem(
-        `${this.CAName}lesson-record`,
-        JSON.stringify(this.lessonRecordList)
+        `${this.CAName}lesson-course`,
+        JSON.stringify(this.renderList)
       );
     },
     changeCA() {
@@ -227,10 +280,11 @@ export default {
     width: 280px;
   }
   .student-list {
-    margin-left: 150px;
+    margin-left: 50px;
 
     .box-card {
       height: 70vh;
+      overflow: auto;
     }
   }
 }
