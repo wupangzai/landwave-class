0e4159f7f432d21fdcb3e363ca00f8e53b956bd9
an_an <1530765818@qq.com>
2024-09-07 21:55:36 +0800

    feat: add select classroom function

--- src/components/class-select.vue
+++ src/components/class-select.vue
@@ -1,17 +1,690 @@
 <template>
-  <div id="demo-select">123456</div>
+  <div id="demo-select">
+    <div class="select-area">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>VIP学生</span>
+          <el-button style="float: right; padding: 3px 0" type="text"
+            >{{ VIPList.length }}条记录</el-button
+          >
+        </div>
+        <div v-for="(stu, index) in VIPList" :key="index" class="text item">
+          <el-tag
+            :type="getColor(VIPList, stu.stuOrClass) >= 2 ? 'success' : ''"
+          >
+            <span>{{ stu.time }}</span>
+            <span style="margin-left: 10px">{{ stu.subject }}</span>
+            <span style="margin-left: 10px">{{ stu.teacher }}</span>
+            <span style="margin-left: 10px">{{ stu.stuOrClass }}</span>
+            <span style="margin-left: 10px">{{ getCAEnName(stu.CA) }}</span>
+          </el-tag>
+          <el-select
+            v-model="stu.classroom"
+            placeholder="请选择"
+            class="selector"
+            size="mini"
+            @change="change"
+          >
+            <el-option
+              v-for="item in classroomOptions"
+              :key="item.label"
+              :label="item.label"
+              :value="item.label"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-card>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>班课</span>
+          <el-button style="float: right; padding: 3px 0" type="text"
+            >{{ classList.length }}条记录</el-button
+          >
+        </div>
+        <div v-for="(stu, index) in classList" :key="index" class="text item">
+          <el-tag type="warning">
+            <span>{{ stu.time }}</span>
+            <span style="margin-left: 10px">{{ stu.subject }}</span>
+            <span style="margin-left: 10px">{{ stu.teacher }}</span>
+            <span style="margin-left: 10px">{{ stu.stuOrClass }}</span>
+            <span style="margin-left: 10px">{{ getCAEnName(stu.CA) }}</span>
+          </el-tag>
+          <el-select
+            v-model="stu.classroom"
+            placeholder="请选择"
+            class="selector"
+            size="mini"
+            @change="change"
+          >
+            <el-option
+              v-for="item in classroomOptions"
+              :key="item.label"
+              :label="item.label"
+              :value="item.label"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-card>
+    </div>
+    <div class="data-area">
+      <el-card class="box-card-data box-card">
+        <div slot="header" class="clearfix">
+          <span
+            style="text-align: center; display: inline-block; width: 100%"
+            >{{ nextDay }}</span
+          >
+        </div>
+        <div class="data-container">
+          <div class="left-class">
+            <div class="cell" v-for="room in renderLeft" :key="room.label">
+              <div class="room">{{ room.label }}</div>
+              <div class="chamber">
+                <div
+                  class="chamber-cell"
+                  :class="getRoomStu(room.label).length <= 1 ? 'onlyone' : ''"
+                  v-for="(stu, index) in getRoomStu(room.label)"
+                  :key="index"
+                >
+                  <span style="margin-left: 5px">{{ stu.time }}</span>
+                  <span
+                    style="
+                      display: inline-block;
+                      width: 285px;
+                      margin-left: 25px;
+                    "
+                    >{{ stu.subject }}</span
+                  >
+                  <span style="margin-left: 15px">{{ stu.teacher }}</span>
+                  <span style="margin-left: 15px">{{ stu.stuOrClass }}</span>
+                  <span style="margin-left: 15px">{{
+                    getCAEnName(stu.CA)
+                  }}</span>
+                </div>
+                <!-- <div class="chamber-cell">123</div>
+                <div class="chamber-cell">123</div>
+                <div class="chamber-cell-end">123</div> -->
+              </div>
+            </div>
+          </div>
+          <div class="left-class">
+            <div class="cell" v-for="room in renderRigth" :key="room.label">
+              <div class="room">{{ room.label }}</div>
+              <div class="chamber">
+                <div
+                  class="chamber-cell"
+                  v-for="(stu, index) in getRoomStu(room.label)"
+                  :key="index"
+                >
+                  <span style="margin-left: 5px">{{ stu.time }}</span>
+                  <span
+                    style="
+                      display: inline-block;
+                      width: 285px;
+                      margin-left: 25px;
+                    "
+                    >{{ stu.subject }}</span
+                  >
+                  <span style="margin-left: 15px">{{ stu.teacher }}</span>
+                  <span style="margin-left: 15px">{{ stu.stuOrClass }}</span>
+                  <span style="margin-left: 15px">{{
+                    getCAEnName(stu.CA)
+                  }}</span>
+                </div>
+                <!-- <div class="chamber-cell">123</div>
+                <div class="chamber-cell">123</div>
+                <div class="chamber-cell-end">123</div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </el-card>
+    </div>
+    <div class="upload">
+      <div>
+        <el-upload
+          class="upload-demo"
+          drag
+          action=""
+          :on-error="fileChangeInfo"
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        </el-upload>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import instance from "../api";
+import moment from "moment";
+import _ from "lodash";
+const Excel = require("exceljs");
+
 export default {
   name: "App",
   data() {
-    return {};
+    return {
+      metaBaseInput: "",
+      renderList: [],
+      classroomOptions: [
+        {
+          label: "VIP1",
+        },
+
+        {
+          label: "VIP2",
+        },
+        {
+          label: "VIP3",
+        },
+        {
+          label: "VIP4",
+        },
+        {
+          label: "VIP5",
+        },
+        {
+          label: "VIP6",
+        },
+        {
+          label: "VIP7",
+        },
+        {
+          label: "VIP8",
+        },
+        {
+          label: "VIP9",
+        },
+        {
+          label: "VIP10",
+        },
+        {
+          label: "VIP11",
+        },
+        {
+          label: "VIP12",
+        },
+        {
+          label: "教室1",
+        },
+        {
+          label: "教室2",
+        },
+        {
+          label: "教室3",
+        },
+        {
+          label: "教室4",
+        },
+        {
+          label: "教室5",
+        },
+        {
+          label: "教室6",
+        },
+        {
+          label: "教室7",
+        },
+        {
+          label: "教室8",
+        },
+        {
+          label: "办公室",
+        },
+      ],
+      renderLeft: [
+        {
+          label: "VIP1",
+        },
+
+        {
+          label: "VIP2",
+        },
+        {
+          label: "VIP3",
+        },
+        {
+          label: "VIP4",
+        },
+        {
+          label: "VIP5",
+        },
+        {
+          label: "VIP6",
+        },
+        {
+          label: "VIP7",
+        },
+        {
+          label: "VIP8",
+        },
+        {
+          label: "VIP9",
+        },
+        {
+          label: "VIP10",
+        },
+        {
+          label: "VIP11",
+        },
+        {
+          label: "VIP12",
+        },
+      ],
+      renderRigth: [
+        {
+          label: "教室1",
+        },
+        {
+          label: "教室2",
+        },
+        {
+          label: "教室3",
+        },
+        {
+          label: "教室4",
+        },
+        {
+          label: "教室5",
+        },
+        {
+          label: "教室6",
+        },
+        {
+          label: "教室7",
+        },
+        {
+          label: "教室8",
+        },
+        {
+          label: "办公室",
+        },
+        {
+          label: " ",
+        },
+        {
+          label: "  ",
+        },
+        {
+          label: "   ",
+        },
+      ],
+      VIPList: [],
+      classList: [],
+      nextDay: moment().add(1, "day").format("YYYY-MM-DD"),
+      CAList: [
+        {
+          value: "吴彬",
+          label: "Anan",
+        },
+        {
+          value: "王淑贤",
+          label: "Yilia",
+        },
+        {
+          value: "刘维倩",
+          label: "Quincy",
+        },
+        {
+          value: "董洁",
+          label: "Jade",
+        },
+        {
+          value: "王南飞",
+          label: "Winnie",
+        },
+      ],
+      dayMap: [
+        {
+          num: 0,
+          day: "Sunday",
+        },
+        {
+          num: 1,
+          day: "Monday",
+        },
+        {
+          num: 2,
+          day: "Tuesday",
+        },
+        {
+          num: 3,
+          day: "Wednesday",
+        },
+        {
+          num: 4,
+          day: "Thursday",
+        },
+        {
+          num: 5,
+          day: "Friday",
+        },
+        {
+          num: 6,
+          day: "Saturday",
+        },
+      ],
+    };
+  },
+  methods: {
+    async getDataBase() {
+      const res = await instance.get(
+        "/myclass/json?parameters=%5B%7B%22type%22%3A%22category%22%2C%22value%22%3A%5B%22%E5%8D%8A%E6%B5%B7%E4%BA%BA%E5%B9%BF%22%5D%2C%22id%22%3A%22e7ab001d-adfb-44aa-7cd3-96ee5f8d0dc2%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22dept%22%5D%5D%7D%2C%7B%22type%22%3A%22date%2Fall-options%22%2C%22value%22%3A%22next1days%22%2C%22id%22%3A%22953e6c0e-7467-721c-8065-fa3451526c25%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22template-tag%22%2C%22date%22%5D%5D%7D%2C%7B%22type%22%3A%22category%22%2C%22value%22%3Anull%2C%22id%22%3A%22627547e3-e078-8cd7-1c44-0038eba685e3%22%2C%22target%22%3A%5B%22variable%22%2C%5B%22template-tag%22%2C%22stuName%22%5D%5D%7D%5D&format_rows=true"
+      );
+      this.metaBaseInput = JSON.stringify(res);
+      this.$message.success("自动获取明日课表成功~");
+    },
+
+    sortByPropertyOrder(arr, property) {
+      const groups = arr.reduce((acc, obj) => {
+        const key = obj[property];
+        acc[key] = acc[key] || [];
+        acc[key].push(obj);
+        return acc;
+      }, {});
+
+      return Object.keys(groups).reduce((acc, key) => {
+        return acc.concat(groups[key]);
+      }, []);
+    },
+
+    // 处理原始数据
+    handleData() {
+      const dataAfterParase = JSON.parse(this.metaBaseInput);
+
+      this.date = moment().add(1, "days").format("YYYY.MM.DD");
+      const dataAfterSorted = this.sortByPropertyOrder(
+        dataAfterParase,
+        "学生/班级"
+      );
+
+      this.renderList = dataAfterSorted.map((item) => {
+        return {
+          time: `${item.start.slice(-5)}-${item.end.slice(-5)}`,
+          subject: item["课程"],
+          stuOrClass: item["学生/班级"],
+          teacher: item["教师"],
+          CA: item["助教"],
+        };
+      });
+      console.log("[  ] >", this.renderList);
+    },
+
+    // 分类，VIP还是班课
+    sortVIPOrClass(stuOrClass) {
+      return this.renderList.filter((item) => {
+        if (stuOrClass === "class") {
+          return item.stuOrClass.length >= 13;
+        }
+        return item.stuOrClass.length < 13;
+      });
+    },
+
+    getVIPAndClassList() {
+      this.VIPList = this.sortVIPOrClass("VIP");
+      this.classList = this.sortVIPOrClass("class");
+    },
+    change() {
+      console.log("[  ] >", this.VIPList);
+    },
+    getCAEnName(cnName) {
+      return this.CAList.find((person) => person.value === cnName)?.label;
+    },
+    getRoomStu(room) {
+      const list = [...this.VIPList, ...this.classList].filter((stu) => {
+        return stu.classroom === room;
+      });
+      return _.sortBy(list, "time");
+    },
+    getColor(list, stuOrClass) {
+      let count = 0;
+      list.forEach((item) => {
+        if (item.stuOrClass === stuOrClass) {
+          count++;
+        }
+      });
+      return count;
+    },
+
+    async fileChangeInfo(e, file) {
+      const stream = file.raw.stream();
+
+      const workbook = new Excel.Workbook();
+
+      const sheetName = moment().add(1, "days").format("YYYY.M.D");
+
+      // 读取文件
+      const res = await workbook.xlsx.read(stream);
+      // 获取第一个工作
+      const worksheet = workbook.getWorksheet(sheetName);
+      console.log("[ worksheet ] >", worksheet);
+
+      const spaceMap = {
+        VIP1: [
+          ["B6", "C6", "D6", "E6", "F6", "G6"],
+          ["B7", "C7", "D7", "E7", "F7", "G7"],
+          ["B8", "C8", "D8", "E8", "F8", "G8"],
+        ],
+        VIP2: [
+          ["B9", "C9", "D9", "E9", "F9", "G9"],
+          ["B10", "C10", "D10", "E10", "F10", "G10"],
+          ["B11", "C11", "D11", "E11", "F11", "G11"],
+        ],
+        VIP3: [
+          ["B12", "C12", "D12", "E12", "F12", "G12"],
+          ["B13", "C13", "D13", "E13", "F13", "G13"],
+          ["B14", "C14", "D14", "E14", "F14", "G14"],
+        ],
+        VIP4: [
+          ["B15", "C15", "D15", "E15", "F15", "G15"],
+          ["B16", "C16", "D16", "E16", "F16", "G16"],
+          ["B17", "C17", "D17", "E17", "F17", "G17"],
+        ],
+        VIP5: [
+          ["B18", "C18", "D18", "E18", "F18", "G18"],
+          ["B19", "C19", "D19", "E19", "F19", "G19"],
+          ["B20", "C20", "D20", "E20", "F20", "G20"],
+        ],
+        VIP6: [
+          ["B21", "C21", "D21", "E21", "F21", "G21"],
+          ["B22", "C22", "D22", "E22", "F22", "G22"],
+          ["B23", "C23", "D23", "E23", "F23", "G23"],
+        ],
+        VIP7: [
+          ["B24", "C24", "D24", "E24", "F24", "G24"],
+          ["B25", "C25", "D25", "E25", "F25", "G25"],
+          ["B26", "C26", "D26", "E26", "F26", "G26"],
+        ],
+        VIP8: [
+          ["B27", "C27", "D27", "E27", "F27", "G27"],
+          ["B28", "C28", "D28", "E28", "F28", "G28"],
+          ["B29", "C29", "D29", "E29", "F29", "G29"],
+        ],
+        VIP9: [
+          ["B30", "C30", "D30", "E30", "F30", "G30"],
+          ["B31", "C31", "D31", "E31", "F31", "G31"],
+          ["B32", "C32", "D32", "E32", "F32", "G32"],
+        ],
+        VIP10: [
+          ["B33", "C33", "D33", "E33", "F33", "G33"],
+          ["B34", "C34", "D34", "E34", "F34", "G34"],
+        ],
+        VIP11: [
+          ["B35", "C35", "D35", "E35", "F35", "G35"],
+          ["B36", "C36", "D36", "E36", "F36", "G36"],
+        ],
+        VIP12: [
+          ["B37", "C37", "D37", "E37", "F37", "G37"],
+          ["B38", "C38", "D38", "E38", "F38", "G38"],
+        ],
+      };
+
+      Object.keys(spaceMap).forEach((item, index) => {
+        const list = [...this.VIPList, ...this.classList].filter((stu) => {
+          return stu.classroom === item;
+        });
+        list.forEach((stu, i) => {
+          worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
+            row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+              spaceMap[item][i].forEach((space, cellIndex) => {
+                if (cell.address === space) {
+                  if (cellIndex === 0) {
+                    cell.value = stu.time;
+                  }
+                  if (cellIndex === 1) {
+                    cell.value = stu.subject;
+                  }
+                  if (cellIndex === 2) {
+                    cell.value = stu.teacher;
+                  }
+                  if (cellIndex === 3) {
+                    cell.value = stu.stuOrClass;
+                  }
+                  if (cellIndex === 4) {
+                    cell.value = this.getCAEnName(stu.CA);
+                  }
+                  if (cellIndex === 5) {
+                    cell.value = this.getCAEnName(stu.CA);
+                  }
+                }
+              });
+            });
+          });
+        });
+        console.log("[ list ] >", list);
+      });
+
+      worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
+        row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+          if (cell.address === "A2") {
+            // 日期修改
+            cell.value = moment().add(1, "days").format("YYYY/M/D");
+          }
+          if (cell.address === "G2") {
+            //星期修改
+            cell.value = this.dayMap.find(
+              (day) => day.num === moment().add(1, "days").day()
+            ).day;
+          }
+        });
+      });
+      workbook.xlsx.writeBuffer().then((data) => {
+        const blob = new Blob([data], { type: "" });
+        saveAs(blob, `${moment().add(1, "days").format("M.D")}教室表.xlsx`);
+      });
+    },
+  },
+  async created() {
+    await this.getDataBase(); // 获取数据
+    this.handleData(); // 处理数据
+    this.getVIPAndClassList();
+    [37, 38].forEach((item) => {
+      console.log(
+        `"B${item}","C${item}","D${item}","E${item}","F${item}","G${item}"`
+      );
+    });
   },
 };
 </script>
 
-<style>
+<style scoped lang="less">
 #demo-select {
+  display: flex;
+  flex-direction: column;
+  // justify-content: center;
+  // align-items: center;
+  .select-area {
+    display: flex;
+
+    .box-card {
+      width: 45vw;
+    }
+  }
+
+  .data-area {
+    width: 100%;
+    margin-top: 10px;
+
+    .box-card {
+      width: calc(90vw + 25px);
+    }
+    .data-container {
+      display: flex;
+      // font-size: 13px;
+
+      .left-class {
+        flex: 1;
+        .cell {
+          display: flex;
+          height: 100px;
+          border: 1px solid black;
+          display: flex;
+          align-items: center;
+          .room {
+            width: 70px;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: rgb(147, 214, 147);
+            border-right: 1px solid black;
+          }
+          .chamber {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+
+            .chamber-cell {
+              border-bottom: 1px solid black;
+              height: 25px;
+              // line-height: 35px;
+            }
+            .onlyone {
+              border-bottom: none;
+            }
+
+            .chamber-cell-end {
+              border-bottom: none;
+              height: 25px;
+              // line-height: 35px;
+            }
+          }
+        }
+      }
+      .right-class {
+        flex: 1;
+      }
+    }
+  }
+
+  .upload-demo {
+    margin-left: 25px;
+  }
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.selector {
+  width: 100px;
+  height: 35px;
+  margin-left: 10px;
 }
 </style>
